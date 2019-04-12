9f9ab97c49d3909ac3f93414b9e710a28ceec541
NganCao <en.pyrish.ym@gmail.com>
2019-04-13 01:19:22 +0700

    bổ sung layout cuối kỳ

--- Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/adapter/GiohangAdapter.java
+++ Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/adapter/GiohangAdapter.java
@@ -1,4 +1,12 @@
 package com.th.thuhien.plantshop.adapter;
 
+import android.content.Context;
+
+import com.th.thuhien.plantshop.model.SanPham;
+
+import java.util.ArrayList;
+
 public class GiohangAdapter {
+    Context context;
+    ArrayList<SanPham> arraySanPham;
 }
