3957815a8f5b4a7351af84de410edf7145f722c1
NganCao <en.pyrish.ym@gmail.com>
2019-04-18 14:02:14 +0700

    main activity

---
+++ BT_tuan6/QuanLyMonHoc/app/src/main/java/com/example/quanlymonhoc/Database.java
@@ -0,0 +1,14 @@
+package com.example.quanlymonhoc;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+//public class Database extends SQLiteOpenHelper
+//{
+//    private static final String TB_MONHOC = "tbMonHoc";
+//
+//    public Database(Context context) {
+//        super(context, DB_NAME, null, DB);
+//    }
+//}
--- Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/activity/GioHangActivity.java
+++ Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/activity/GioHangActivity.java
@@ -1,15 +1,43 @@
 package com.th.thuhien.plantshop.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.th.thuhien.plantshop.R;
+import com.th.thuhien.plantshop.model.GioHang;
 
 public class GioHangActivity extends AppCompatActivity {
 
+    Button btnPrev, btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gio_hang);
+    }
+
+    private void setControl() {
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+    }
+
+    private void setEvent(){
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent();
+                finish();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GioHangActivity.this, CheckOut.class);
+                startActivity(intent);
+            }
+        });
     }
 }
