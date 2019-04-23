39822089e49d91dca3a6939143d6bdc70069972a
NganCao <en.pyrish.ym@gmail.com>
2019-04-23 10:55:39 +0700

    error - ngan

--- BT_GK/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BT_GK/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,13 @@
-package com.example.quanlysinhvien;
+package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SinhVien extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sinh_vien);
+        setContentView(R.layout.activity_main);
     }
 }
