88468fd808ac6e2d9052fc0868418732f89b4cab
NganCao <en.pyrish.ym@gmail.com>
2019-04-24 15:52:15 +0700

    button back - ngan

--- BT_GK/app/src/main/java/com/example/myapplication/DSDiem.java
+++ BT_GK/app/src/main/java/com/example/myapplication/DSDiem.java
@@ -1,13 +1,46 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class DSDiem extends AppCompatActivity {
+
+    ListView listView;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dsdiem);
+
+        setControl();
+        setEvent();
+    }
+
+    public void setControl(){
+        listView = (ListView) findViewById(R.id.listView);
+        btnBack = (Button) findViewById(R.id.btnBack);
+    }
+
+    public void setEvent(){
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return false;
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DSDiem.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
