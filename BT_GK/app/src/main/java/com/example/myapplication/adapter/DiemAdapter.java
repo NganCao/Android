3f636b7f15209755162f108b2d0a47b2929735b4
NganCao <en.pyrish.ym@gmail.com>
2019-04-24 15:30:17 +0700

    adapter sv + diem, H update 2 adapter vaf class MH + LOP - ngan

--- BT_GK/app/src/main/java/com/example/myapplication/adapter/DiemAdapter.java
+++ BT_GK/app/src/main/java/com/example/myapplication/adapter/DiemAdapter.java
@@ -1,4 +1,73 @@
 package com.example.myapplication.adapter;
 
-public class DiemAdapter {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.myapplication.R;
+
+import com.example.myapplication.model.Diem;
+
+
+import java.util.ArrayList;
+
+public class DiemAdapter extends ArrayAdapter<Diem> {
+    Activity context;
+    int layoutId;
+    ArrayList<Diem> data;
+
+    public DiemAdapter (Activity context, int layoutId, ArrayList<Diem> data){
+        super(context, layoutId, data);
+        this.context = context;
+        this.layoutId = layoutId;
+        this.data = data;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        Holder holder = null;
+        if(row == null){
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutId, parent,false);
+
+            holder = new Holder();
+            holder.txtMamh = (TextView) row.findViewById(R.id.txtMamh);
+            holder.txtMasv = (TextView) row.findViewById(R.id.txtMasv);
+            holder.txtDiem = (TextView) row.findViewById(R.id.txtDiem);
+
+            row.setTag(holder);
+        }
+        else {
+            holder = (Holder) row.getTag();
+        }
+
+        Diem diem = data.get(position);
+        holder.txtMamh.setText("Mã MH: ");
+        holder.txtMasv.setText("Mã SV: " + diem.getSinhVien().getMa());
+        holder.txtDiem.setText("Điểm: " + diem.getDiem());
+        return row;
+
+//        Holder holder = null;
+//        convertView = context.getLayoutInflater().inflate(layoutId, null);
+//
+//        holder = new Holder();
+//            holder.txtMamh = (TextView) convertView.findViewById(R.id.txtMamh);
+//            holder.txtMasv = (TextView) convertView.findViewById(R.id.txtMasv);
+//            holder.txtDiem = (TextView) convertView.findViewById(R.id.txtDiem);
+//
+//        Diem diem = data.get(position);
+//        holder.txtMamh.setText("Mã MH: ");
+//        holder.txtMasv.setText("Mã SV: " + diem.getSinhVien().getMa());
+//        holder.txtDiem.setText("Điểm: " + diem.getDiem());
+//
+//        return convertView;
+    }
+
+    static class Holder{
+        TextView txtMasv, txtMamh, txtDiem;
+    }
+
 }
--- BT_GK/app/src/main/java/com/example/myapplication/adapter/SinhVienAdapter.java
+++ BT_GK/app/src/main/java/com/example/myapplication/adapter/SinhVienAdapter.java
@@ -1,4 +1,50 @@
 package com.example.myapplication.adapter;
 
-public class SinhVienAdapter {
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.myapplication.R;
+import com.example.myapplication.model.SinhVien;
+
+import java.util.ArrayList;
+
+public class SinhVienAdapter extends ArrayAdapter<SinhVien> {
+    Activity context;
+    int layoutId;
+    ArrayList<SinhVien> data;
+
+    public SinhVienAdapter(Activity context, int layoutId, ArrayList<SinhVien> data){
+        super(context, layoutId, data);
+        this.context = context;
+        this.layoutId = layoutId;
+        this.data = data;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        convertView = context.getLayoutInflater().inflate(layoutId, null);
+
+        Holder holder = new Holder();
+        holder.txtMasv = (TextView) convertView.findViewById(R.id.txtMasv);
+        holder.txtMalop = (TextView) convertView.findViewById(R.id.txtMalop);
+        holder.txtTen = (TextView) convertView.findViewById(R.id.txtTen);
+        holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+
+        SinhVien sv = data.get(position);
+        holder.txtMalop.setText("Mã lớp: "+ sv.getLop());
+        holder.txtMasv.setText("Mã SV: "+ sv.getMa());
+        holder.txtTen.setText("Họ tên: "+ sv.getHo()+" "+ sv.getTen());
+        holder.icon.setImageResource(R.drawable.boy);
+        if (!sv.isPhai())
+            holder.icon.setImageResource(R.drawable.girl);
+
+        return convertView;
+    }
+
+    static class Holder{
+        TextView txtMalop, txtMasv, txtTen;
+        ImageView icon;
+    }
 }
