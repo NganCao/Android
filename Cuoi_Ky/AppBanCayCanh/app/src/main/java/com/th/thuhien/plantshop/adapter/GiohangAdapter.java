734f51a0a415892bbb15710104319cf45f5f1955
NganCao <en.pyrish.ym@gmail.com>
2019-05-02 15:04:32 +0700

    gio hang activities

--- Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/adapter/GiohangAdapter.java
+++ Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/adapter/GiohangAdapter.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.th.thuhien.plantshop.R;
+import com.th.thuhien.plantshop.activity.GioHangActivity;
+import com.th.thuhien.plantshop.activity.MainActivity;
 import com.th.thuhien.plantshop.model.GioHang;
 import com.th.thuhien.plantshop.model.SanPham;
 
@@ -50,7 +52,7 @@ public class GiohangAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         GHHolder holder = null;
         if (holder == null){
             holder = new GHHolder();
@@ -67,7 +69,7 @@ public class GiohangAdapter extends BaseAdapter {
         else {
             holder = (GHHolder) convertView.getTag();
         }
-        GioHang gioHang = (GioHang) getItem(position);
+        final GioHang gioHang = (GioHang) getItem(position);
         holder.txtName.setText(gioHang.getTenSP());
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
         holder.txtGia.setText(decimalFormat.format(gioHang.getGiaSP())+" vnd");
@@ -75,7 +77,70 @@ public class GiohangAdapter extends BaseAdapter {
                 .placeholder(R.drawable.product)
                 .error(R.drawable.error)
                 .into(holder.icon);
-        holder.quantity.setText(gioHang.soluongSP);
-        return null;
+        holder.quantity.setText(gioHang.soluongSP + "");
+        final int sl = Integer.parseInt(holder.quantity.getText().toString());
+        if( sl >=10){
+            holder.add.setVisibility(View.INVISIBLE);
+            holder.del.setVisibility(View.VISIBLE);
+        }
+        else if(sl<=1){
+            holder.del.setVisibility(View.INVISIBLE);
+        }
+        else if(sl>=1){
+            holder.add.setVisibility(View.VISIBLE);
+            holder.del.setVisibility(View.VISIBLE);
+        }
+        final GHHolder finalHolder = holder;
+        holder.add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int slnew = Integer.parseInt(finalHolder.quantity.getText().toString())+1;
+                int slht = MainActivity.arrayGioHang.get(position).getSoluongSP();
+                long giaht = MainActivity.arrayGioHang.get(position).getGiaSP();
+                MainActivity.arrayGioHang.get(position).setSoluongSP(slnew);
+                long gianew = (giaht * slnew) / slht;
+                MainActivity.arrayGioHang.get(position).setGiaSP(gianew);
+                DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+                finalHolder.txtGia.setText(decimalFormat.format(gianew)+" vnd");
+                GioHangActivity.EvenUltil();
+                if (slnew > 9 ){
+                    finalHolder.add.setVisibility(View.INVISIBLE);
+                    finalHolder.del.setVisibility(View.VISIBLE);
+                    finalHolder.quantity.setText(String.valueOf(slnew));
+                }
+                else{
+                    finalHolder.add.setVisibility(View.VISIBLE);
+                    finalHolder.del.setVisibility(View.VISIBLE);
+                    finalHolder.quantity.setText(String.valueOf(slnew));
+                }
+            }
+        });
+
+        holder.del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int slnew = Integer.parseInt(finalHolder.quantity.getText().toString())-1;
+                int slht = MainActivity.arrayGioHang.get(position).getSoluongSP();
+                long giaht = MainActivity.arrayGioHang.get(position).getGiaSP();
+                MainActivity.arrayGioHang.get(position).setSoluongSP(slnew);
+                long gianew = (giaht * slnew) / slht;
+                MainActivity.arrayGioHang.get(position).setGiaSP(gianew);
+                DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+                finalHolder.txtGia.setText(decimalFormat.format(gianew)+" vnd");
+                GioHangActivity.EvenUltil();
+                if (slnew < 2 ){
+                    finalHolder.del.setVisibility(View.INVISIBLE);
+                    finalHolder.del.setVisibility(View.VISIBLE);
+                    finalHolder.quantity.setText(String.valueOf(slnew));
+                }
+                else{
+                    finalHolder.add.setVisibility(View.VISIBLE);
+                    finalHolder.del.setVisibility(View.VISIBLE);
+                    finalHolder.quantity.setText(String.valueOf(slnew));
+                }
+            }
+        });
+
+        return convertView;
     }
 }
