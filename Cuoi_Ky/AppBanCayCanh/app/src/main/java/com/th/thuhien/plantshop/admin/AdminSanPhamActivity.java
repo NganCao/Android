841dd9dda79fdee46f2df7ff03ed87912190bb4e
luonghien347 <=>
2019-05-04 18:24:17 +0700

    quan tri: show san pham theo menu - spinner

--- Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/admin/AdminSanPhamActivity.java
+++ Cuoi_Ky/AppBanCayCanh/app/src/main/java/com/th/thuhien/plantshop/admin/AdminSanPhamActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.ToolbarWidgetWrapper;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.th.thuhien.plantshop.R;
 import com.th.thuhien.plantshop.adapter.ManHinhSanPhamAdapter;
@@ -22,6 +26,7 @@ import java.util.List;
 public class AdminSanPhamActivity extends AppCompatActivity {
 
     Toolbar toolbarAdminSanPham;
+    TextView tv_AdminTenMenu;
     ListView lv_AdminSanPham;
     Spinner spinnerListMenu;
 
@@ -37,22 +42,54 @@ public class AdminSanPhamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_san_pham);
 
         AnhXa();
+        setEventSelectItemSpinner();
         getListMenu();
-        getListSanPham();
+
+
+    }
+
+
+    private void setEventSelectItemSpinner() {
+        spinnerListMenu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Log.d("pos", data.get(position).getTenSp());
+                if (position == 0){
+                    tv_AdminTenMenu.setText("Danh sách tất cả các cây");
+                    data.clear();
+                    getListSP();
+                }else {
+                    tv_AdminTenMenu.setText("Danh sách các " + listMenu.get(position).getTenMenu());
+                    data.clear();
+                    getListSanPhamTheoMenu(listMenu.get(position).getMaMenu());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private void getListMenu() {
+        listMenu.add(new Menu("All"));
         AsynListAdminMenu asynListAdminMenu = new AsynListAdminMenu();
         asynListAdminMenu.execute();
     }
 
-    private void getListSanPham() {
+    private void getListSP() {
         AsynAdminListSanPham asynAdminListSanPham = new AsynAdminListSanPham();
         asynAdminListSanPham.execute();
     }
 
+    private void getListSanPhamTheoMenu(int pos) {
+        AsynAdminSanPhamTheoMenu asynAdminSanPhamTheoMenu = new AsynAdminSanPhamTheoMenu();
+        asynAdminSanPhamTheoMenu.execute(pos);
+    }
     private void AnhXa() {
         toolbarAdminSanPham = (Toolbar) findViewById(R.id.toolbarAdminSanPham);
+        tv_AdminTenMenu = (TextView) findViewById(R.id.textviewAdminTenMenu);
         lv_AdminSanPham = (ListView) findViewById(R.id.listviewAdminSanPham);
         spinnerListMenu = (Spinner) findViewById(R.id.spinnerListMenu);
 
@@ -63,6 +100,7 @@ public class AdminSanPhamActivity extends AppCompatActivity {
         listMenu = new ArrayList<>();
         menuAdapter = new MenuAdapter(listMenu, getApplicationContext());
         spinnerListMenu.setAdapter(menuAdapter);
+
     }
 
     public class AsynAdminListSanPham extends AsyncTask<Void, Void, List<SanPham>>{
@@ -98,9 +136,26 @@ public class AdminSanPhamActivity extends AppCompatActivity {
             for (int i = 0; i < menu.size(); i++){
                 listMenu.add(menu.get(i));
             }
-            listMenu.add(new Menu("All"));
             menuAdapter.notifyDataSetChanged();
         }
     }
 
+    public class AsynAdminSanPhamTheoMenu extends AsyncTask<Integer, Void, List<SanPham>>{
+
+
+        @Override
+        protected List<SanPham> doInBackground(Integer... integers) {
+            SanPhamService sanPhamService = new SanPhamService();
+            return sanPhamService.getSanPhamByMenu(integers[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<SanPham> sanPhams) {
+            super.onPostExecute(sanPhams);
+            for (int i = 0; i < sanPhams.size(); i++){
+                data.add(sanPhams.get(i));
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
